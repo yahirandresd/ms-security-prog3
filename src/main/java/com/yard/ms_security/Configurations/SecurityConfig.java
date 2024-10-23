eac883760427a6bc0d70bb6dd164a3b5a79e9b54
Mandara2 <145130423+Mandara2@users.noreply.github.com>
2024-10-22 23:19:49 -0500

    Update SecurityConfig.java

--- src/main/java/com/yard/ms_security/Configurations/SecurityConfig.java
+++ src/main/java/com/yard/ms_security/Configurations/SecurityConfig.java
@@ -2,10 +2,9 @@ package com.yard.ms_security.Configurations;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -14,14 +13,16 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
-                .authorizeHttpRequests(auth -> auth
-                        .anyRequest().authenticated()) // Cualquier otra solicitud debe estar autenticada
+        return http
+                .authorizeHttpRequests(
+                        request -> {
+                            request.requestMatchers("/api/public/security/login", "/api/public/security/login/google").permitAll();
+                            request.anyRequest().authenticated(); // Asegúrate de que otras rutas requieran autenticación
+                        })
+                .csrf(AbstractHttpConfigurer::disable)
                 .oauth2Login(oauth2 -> oauth2
                         .defaultSuccessUrl("/home", true) // Redirige a /home tras un login exitoso
-                        .failureUrl("/login?error=true")); // Redirige a /login si falla el login
-        // Deshabilita CSRF (útil si estás haciendo API REST sin formularios)
-
-        return http.build();
+                        .failureUrl("/login?error=true")) // Redirige a /login si falla el login
+                .build();
     }
 }
